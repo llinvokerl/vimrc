a3148a2d2dde8bd6edb4db9cfaca54a9fbdd66be
llinvokerl <1959600040@qq.com>
2018-06-11 23:07:58 +0800

    加安装ctags 防止插件报错

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
-sudo apt-get update && sudo apt-get install git
+sudo apt-get update && sudo apt-get install git ctags
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
