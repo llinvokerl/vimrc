a762cc1054be18086d059e22c1eba5144ab713f1
llinvokerl <1959600040@qq.com>
2018-10-08 15:42:04 +0800

    修改安装程序

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
-sudo apt-get update && sudo apt-get install git exuberant-ctags -y
+ln -s `pwd`/.vimrc.bundles ~/.vimrc.bundles
+sudo apt-get update && sudo apt-get install git ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -u NONE +PluginInstall +qall
+vim -u `pwd`/.vimrc.bundles +PluginInstall +qall
