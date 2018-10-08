cb59866a097362b56ed8af61795dd7371d6b5d9e
llinvokerl <38915183+llinvokerl@users.noreply.github.com>
2018-10-08 14:42:10 +0800

    Update install.sh
    exuberant-ctags 替换ctags

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
-sudo apt-get update && sudo apt-get install git ctags -y
+sudo apt-get update && sudo apt-get install git exuberant-ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u NONE +PluginInstall +qall
