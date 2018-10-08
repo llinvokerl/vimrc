b8c96012b404ddcadacbc3d94ab31a1ebdd49b20
llinvokerl <38915183+llinvokerl@users.noreply.github.com>
2018-10-08 14:32:43 +0800

    Update install.sh
    apt install 加入-y

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
-sudo apt-get update && sudo apt-get install git ctags
+sudo apt-get update && sudo apt-get install git ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
