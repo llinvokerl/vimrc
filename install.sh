805135d8f007cd503fbbde385437d70b428e4f11
llinvokerl <38915183+llinvokerl@users.noreply.github.com>
2018-10-08 14:38:27 +0800

    Update install.sh
    fix warning

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
 sudo apt-get update && sudo apt-get install git ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+vim -u NONE +PluginInstall +qall
