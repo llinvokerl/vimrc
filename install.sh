e56510e6d7c94a0e424e95f6cd53bf2873d7698e
llinvokerl <1959600040@qq.com>
2018-10-08 11:05:07 +0000

    apt-get移出instal.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$0")"
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vimrc.bundles ~/.vimrc.bundles
-apt-get update && apt-get install git ctags -y
+#apt-get update && apt-get install git ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc.bundles +PluginInstall +qall
