37c188fc276866756271669da95274bb8860b0d6
liu <liu@B-M2H0JHD2-1913.local>
2021-01-29 18:44:47 +0800

    MAINT:feat 增加安装vim-go依赖组件流程

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vimrc.bundles ~/.vimrc.bundles
 #apt-get update && apt-get install git ctags -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -u ~/.vimrc.bundles +PluginInstall +qall
+vim -u ~/.vimrc.bundles +PluginInstall +GoInstallBinaries +qall
